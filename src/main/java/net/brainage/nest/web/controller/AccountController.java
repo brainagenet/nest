fb9d33578712e31d21c90bb6a7e1e4c2a9d78f2a
ms29seo <ms29.seo+java@gmail.com>
2016-02-16 01:56:26 +0900

    사용자 가입 프로세스와 사용자 로그인 프로세스를 위한 form class와 controller class 정리

--- src/main/java/net/brainage/nest/web/controller/AccountController.java
+++ src/main/java/net/brainage/nest/web/controller/AccountController.java
@@ -18,13 +18,76 @@
  */
 package net.brainage.nest.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import net.brainage.nest.web.form.SigninForm;
+import net.brainage.nest.web.form.SignupForm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author <a href="mailto:ms29.seo@gmail.com">ms29.seo</a>
  */
+@Slf4j
 @Controller
 @RequestMapping(path = {"/account"})
 public class AccountController {
+
+    @RequestMapping(path = "/signup", method = RequestMethod.GET)
+    public String signupForm(Model model) {
+        return "account/signupForm";
+    }
+
+    @RequestMapping(path = "/signup", method = RequestMethod.POST)
+    public String signupAction(SignupForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute(form);
+            return signinForm(model);
+        }
+
+        // TODO: 2-factor authentication이 활성화 된다면 QR Code URI로 Redirect 한다.
+        boolean enable2FactorAuth = false;
+        if (enable2FactorAuth) {
+            return "redirect:/account/signup/opt/qrcode";
+        }
+        return "redirect:/account/signin";
+    }
+
+    @RequestMapping(path = "/signin", method = RequestMethod.GET)
+    public String signinForm(Model model) {
+        return "account/signinForm";
+    }
+
+    @RequestMapping(path = "/signin", method = RequestMethod.POST)
+    public String signinAction(SigninForm form, BindingResult result, Model model) {
+        // username과 password를 비교하여 인증을 한다.
+
+        // 2-factor authentication이 활성화 되어 있다면 otp 확인 form으로 redirect 한다.
+        boolean enable2FactorAuth = false;
+        if (enable2FactorAuth) {
+            return "redirect:/account/signin/otp";
+        }
+
+        // 2-factor authentication이 비활성화 되어 있다면 next 값이 존재하면 next uri로 redirect 하고
+        // 그렇지 않다면 기본 main 화면으로 redirect 한다.
+        if (StringUtils.hasText(form.getNext())) {
+            return "redirect:" + form.getNext();
+        }
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/signin/opt", method = RequestMethod.GET)
+    public String signinOtpForm(Model model) {
+        return "account/signinOtpForm";
+    }
+
+    @RequestMapping(path = "/signin/otp", method = RequestMethod.POST)
+    public String signinOtpAction(SigninForm form, BindingResult result, Model model) {
+        return "redirect:/";
+    }
+
 }
